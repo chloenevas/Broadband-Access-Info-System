aa62679226f8601ae99492302cc913adb84f09a3
KammieD <Kamryn_walker@brown.edu>
2023-10-24 01:41:30 -0400

    chore: added backend files to project

--- target/classes/README.md
+++
@@ -1,1 +0,0 @@
-# REPL-cnevas-kwalke19
--- target/classes/target/classes/repl/src/components/constants.ts
+++
@@ -1,8 +0,0 @@
-// When we write tests, we'll be searching using accessible names. So let's
-// use the same constant identifier; that way if we decide to change the text
-// in the app, it won't break our tests.
-export const TEXT_try_button_accessible_name = "submit your command";
-export const TEXT_input_box = "enter command";
-export const TEXT_try_button_text = "Submit!";
-
-
