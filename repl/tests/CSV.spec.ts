bce994b713be636694fc3a906111dff47830dd2b
KammieD <Kamryn_walker@brown.edu>
2023-10-26 17:18:54 -0400

    test: one test added for state change from header/no header

--- repl/tests/CSV.spec.ts
+++ repl/tests/CSV.spec.ts
@@ -409,6 +409,137 @@ test("MOCKED: if I load a file without a header and then view, I get the correct
   ).toBeVisible;
 });
 
+
+test("MOCKED: If I load a file with a header and then one without, I get the correct table", async ({
+  page,
+}) => {
+  await page.getByLabel(TEXT_input_box).click();
+  await page.getByLabel(TEXT_input_box).fill("mock");
+  await page.getByRole("button").click();
+  await page.getByLabel(TEXT_input_box).click();
+  await page
+    .getByLabel(TEXT_input_box)
+    .fill(
+      "load_file /Users/chloenevas/Documents/mock-cnevas-rgonza27/mock/src/components/data/income.csv"
+    );
+  await page.getByRole("button").click();
+  await page.getByLabel(TEXT_input_box).click();
+  await page.getByLabel(TEXT_input_box).fill("mock");
+  await page.getByRole("button").click();
+  await page.getByLabel(TEXT_input_box).click();
+  await page
+    .getByLabel(TEXT_input_box)
+    .fill(
+      "load_file /Users/chloenevas/Documents/mock-cnevas-rgonza27/mock/src/components/data/stars.csv"
+    );
+  await page.getByRole("button").click();
+  await page.getByLabel(TEXT_input_box).click();
+  await page.getByLabel(TEXT_input_box).fill("view");
+  await page.getByRole("button").click();
+
+  await expect(page.getByRole("table")).toBeVisible();
+
+  await expect(page.getByRole("row", { name: "0 Sol 0 0 0" })).toBeVisible;
+  await expect(page.getByRole("row", { name: "1 282.43485 0.00449 5.36884" }))
+    .toBeVisible;
+  await expect(page.getByRole("row", { name: "2 43.04329 0.00285 -15.24144" }))
+    .toBeVisible;
+  await expect(page.getByRole("row", { name: "3 277.11358 0.02422 223.27753" }))
+    .toBeVisible;
+  await expect(
+    page.getByRole("row", { name: "3759 96 G. Psc 7.26388 1.55643 0.68697" })
+  ).toBeVisible;
+  await expect(
+    page.getByRole("row", {
+      name: "70667 Proxima Centauri -0.47175 -0.36132 -1.15037",
+    })
+  ).toBeVisible;
+  await expect(
+    page.getByRole("row", {
+      name: "71454 Rigel Kentaurus B -0.50359 -0.42128 -1.1767",
+    })
+  ).toBeVisible;
+  await expect(
+    page.getByRole("row", {
+      name: "71457 Rigel Kentaurus A -0.50362 -0.42139 -1.17665",
+    })
+  ).toBeVisible;
+  await expect(
+    page.getByRole("row", {
+      name: "87666 Barnard's Star -0.01729 -1.81533 0.14824",
+    })
+  ).toBeVisible;
+  await expect(
+    page.getByRole("row", { name: "118721 -2.28262 0.64697 0.20354" })
+  ).toBeVisible;
+});
+
+test("MOCKED: If I load a file without a header and then one with, I get the correct table", async ({
+  page,
+}) => {
+  await page.getByLabel(TEXT_input_box).click();
+  await page.getByLabel(TEXT_input_box).fill("mock");
+  await page.getByRole("button").click();
+  await page.getByLabel(TEXT_input_box).click();
+  await page
+    .getByLabel(TEXT_input_box)
+    .fill(
+      "load_file /Users/chloenevas/Documents/mock-cnevas-rgonza27/mock/src/components/data/stars.csv"
+    );
+  await page.getByRole("button").click();
+
+  await page.getByLabel(TEXT_input_box).click();
+  await page.getByLabel(TEXT_input_box).fill("mock");
+  await page.getByRole("button").click();
+  await page.getByLabel(TEXT_input_box).click();
+  await page
+    .getByLabel(TEXT_input_box)
+    .fill(
+      "load_file /Users/chloenevas/Documents/mock-cnevas-rgonza27/mock/src/components/data/stars.csv"
+    );
+  await page.getByRole("button").click();
+  await page.getByLabel(TEXT_input_box).click();
+  await page.getByLabel(TEXT_input_box).fill("view");
+  await page.getByRole("button").click();
+
+  await expect(page.getByRole("table")).toBeVisible();
+
+  await expect(page.getByRole("row", { name: "0 Sol 0 0 0" })).toBeVisible;
+  await expect(page.getByRole("row", { name: "1 282.43485 0.00449 5.36884" }))
+    .toBeVisible;
+  await expect(page.getByRole("row", { name: "2 43.04329 0.00285 -15.24144" }))
+    .toBeVisible;
+  await expect(page.getByRole("row", { name: "3 277.11358 0.02422 223.27753" }))
+    .toBeVisible;
+  await expect(
+    page.getByRole("row", { name: "3759 96 G. Psc 7.26388 1.55643 0.68697" })
+  ).toBeVisible;
+  await expect(
+    page.getByRole("row", {
+      name: "70667 Proxima Centauri -0.47175 -0.36132 -1.15037",
+    })
+  ).toBeVisible;
+  await expect(
+    page.getByRole("row", {
+      name: "71454 Rigel Kentaurus B -0.50359 -0.42128 -1.1767",
+    })
+  ).toBeVisible;
+  await expect(
+    page.getByRole("row", {
+      name: "71457 Rigel Kentaurus A -0.50362 -0.42139 -1.17665",
+    })
+  ).toBeVisible;
+  await expect(
+    page.getByRole("row", {
+      name: "87666 Barnard's Star -0.01729 -1.81533 0.14824",
+    })
+  ).toBeVisible;
+  await expect(
+    page.getByRole("row", { name: "118721 -2.28262 0.64697 0.20354" })
+  ).toBeVisible;
+  
+});
+
 test("I can load + view and then repeat with a different file", async ({
   page,
 }) => {
